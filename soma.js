5b332c1b3627f3ac97fcb548df61386ce2fc13f2
Gabriel Ramalho <gaboramalho@gmail.com>
2022-09-26 20:08:29 -0300

    Ainda são necessários ajustes

---
+++ soma.js
@@ -0,0 +1,17 @@
+numero = 10000;
+
+function less() {
+  numero--;
+  setValue(numero);
+}
+
+function more() {
+  numero++;
+  setValue(numero);
+}
+
+function setValue(value) {
+  document.getElementById('num').value = value;
+}
+
+setValue(numero);
