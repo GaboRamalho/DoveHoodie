76e8188c551cc7b006b2ee752631cf859949bc97
Gabriel Ramalho <gaboramalho@gmail.com>
2022-09-30 18:53:24 -0300

    Final Version. It's finally done!

--- mainshoppingcart.js
+++ mainshoppingcart.js
@@ -2,6 +2,6 @@ var valor = 2800.00; //variavel com o valor do produto
 
 function productvalue() //criacao da funcao
 {
-    var valorQuantidade = this.valor * parseFloat(document.getElementById("quantidade").value); //variavel que vai pegar o valor do produto e multiplicar pela quantidade
-	document.getElementById("totalvalor").innerHTML = (valorQuantidade); //aqui deve retornar o valor
+    var valorQuantidade = this.valor * parseFloat(document.getElementById("amount").value); //variavel que vai pegar o valor do produto e multiplicar pela quantidade
+	document.getElementById("totalvalue").innerHTML = (valorQuantidade); //aqui deve retornar o valor
 }
